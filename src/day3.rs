52d416c7564b157a479c5e78e70f052540013677
Brian Hang <hangwithbrian@gmail.com>
2022-12-03 02:02:15 -0800

    Day 3 Part 2

--- src/day3.rs
+++ src/day3.rs
@@ -11,30 +11,34 @@ fn main() {
         File::open(&input_path).expect(&format!("{} is not a valid file!", input_path.display()));
     let buf = BufReader::new(file);
 
-    let total_priority: u32 = buf
-        .lines()
-        .map(|line| {
-            let line = line.expect("Failed to read a line!");
-            let mut items1: Vec<char> = line.chars().collect();
-            let items2 = items1.split_off(items1.len() / 2);
+    let lines: Vec<String> = buf.lines().map(|line| line.unwrap()).collect();
+    let groups = lines.chunks(3);
 
-            let items1_set: HashSet<char> = HashSet::from_iter(items1);
-            let items2_set: HashSet<char> = HashSet::from_iter(items2);
+    let total_priority: u32 = groups
+        .map(|lines| {
+            let items: Vec<HashSet<char>> = lines
+                .into_iter()
+                .map(|line| HashSet::from_iter(line.chars()))
+                .collect();
 
-            let dupes = items1_set.intersection(&items2_set);
-
-            let dupe_priority: u32 = dupes.map(get_priority).sum();
-            return dupe_priority;
+            if let Some((first_items, rest_items)) = items.split_first() {
+                let mut first_items = first_items.clone();
+                for other_items in rest_items {
+                    first_items.retain(|item| other_items.contains(item));
+                }
+                return first_items.into_iter().map(get_priority).sum();
+            }
+            return 0;
         })
         .sum();
 
     println!("{}", total_priority);
 }
 
-fn get_priority(item: &char) -> u32 {
-    match *item {
-        'a'..='z' => (*item as u32) - ('a' as u32) + 1,
-        'A'..='Z' => (*item as u32) - ('A' as u32) + 27,
+fn get_priority(item: char) -> u32 {
+    match item {
+        'a'..='z' => (item as u32) - ('a' as u32) + 1,
+        'A'..='Z' => (item as u32) - ('A' as u32) + 27,
         _ => panic!("invalid item: {}", item),
     }
 }
